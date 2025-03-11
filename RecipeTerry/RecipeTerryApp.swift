72f6a551cf0557edf62efcb715a3796cf4a69577
terry <terry@skimpon.com>
2025-03-11 13:17:18 -0500

    first commit

---
+++ RecipeTerry/RecipeTerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTerryApp.swift
+//  RecipeTerry
+//
+//  Created by Terry Lee on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeTerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeTerryTests/RecipeTerryTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTerryTests.swift
+//  RecipeTerryTests
+//
+//  Created by Terry Lee on 3/11/25.
+//
+
+import Testing
+@testable import RecipeTerry
+
+struct RecipeTerryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
